794b38662cd246f9fe3dedad874f7650d6e2ada2
DalatCoder <hieuntctk42@gmail.com>
2019-06-15 17:27:40 +0700

    Catch error

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const request = require('request')
 const latitude = 11.9600327
 const longitude = 108.4394856
 const units = 'si'
+const darkSkyToken = 'fee3e6e79b9a143d6a7f3a639e14c4a1'
 
-const url = `https://api.darksky.net/forecast/fee3e6e79b9a143d6a7f3a639e14c4a1/${latitude},${longitude}?units=${units}`
+const darkSkyURL = `https://api.darksky.net/forecast/${darkSkyToken}/${latitude},${longitude}?units=${units}`
 
-request({ url: url, json: true }, (error, response) => {
-  const data = response.body
 
-  console.log(`${data.daily.summary} It is currently ${data.currently.temperature} degrees out. There is a ${data.currently.precipProbability*100}% chance of rain.`)
+// request({ url: darkSkyURL, json: true }, (error, response) => {
+//   const data = response.body
+
+//   console.log(`${data.daily.summary} It is currently ${data.currently.temperature} degrees out. There is a ${data.currently.precipProbability*100}% chance of rain.`)
+// })
+
+const geoCodeToken = 'pk.eyJ1IjoidHJvbmdoaWV1IiwiYSI6ImNqd3hicWk5djBpNmYzeWp6ZXp1c3N3NHcifQ.kr1qW_AQ3AwdgPPFHqjI5Q'
+const address = 'Los%20Angeles'
+
+const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${geoCodeToken}&limit=1`
+
+request({ url: geoCodeURL, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to mapbox services!')
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to find location!')
+  } else {
+    const longitude = response.body.features[0].center[0]
+    const latitude = response.body.features[0].center[1]
+    console.log(`${latitude} : ${longitude}`)
+  }
 })
