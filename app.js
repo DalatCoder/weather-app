5c7145755fefeb6e7ec91d5fc824e8d32726e1af
DalatCoder <hieuntctk42@gmail.com>
2019-06-16 14:33:52 +0700

    Return pattern

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const request = require('request')
 const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const address = 'Da Lat'
+const address = process.argv[2]
+
+if (address === '' || address === undefined) {
+  return console.log('Please enter location!')
+}
 
 geoCode(address, (error, geocode) => {
   if (error) {
-    console.log(error)
-  } else {
-    console.log(geocode.location)
-
-    forecast(geocode.latitude, geocode.longitude, (error, data) => {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log(`${data.summary} It's ${data.temperature} degrees out. It have ${data.rainPercent}% chance of rain.`)
-      }
-    })
+    return console.log(error)
   }
+
+  forecast(geocode.latitude, geocode.longitude, (error, data) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log(geocode.location)
+    console.log(`${data.summary} It's ${data.temperature} degrees out. There is ${data.rainPercent}% chance of rain.`)
+  })
 })
