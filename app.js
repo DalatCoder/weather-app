43d443cac18543f41751b50680e1b16fcdc47177
DalatCoder <hieuntctk42@gmail.com>
2019-06-16 08:47:47 +0700

    Callback pattern

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 const request = require('request')
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const latitude = 11.9600327
-const longitude = 108.4394856
-const units = 'si'
-const darkSkyToken = 'fee3e6e79b9a143d6a7f3a639e14c4a1'
+const address = 'Da Lat'
 
-const darkSkyURL = `https://api.darksky.net/forecast/${darkSkyToken}/${latitude},${longitude}?units=${units}`
-
-
-request({ url: darkSkyURL, json: true }, (error, response) => {
-
+geoCode(address, (error, geocode) => {
   if (error) {
-    console.log('Unable to connect to services!')
-  } else if (response.body.error) {
-    console.log('Unable to find location!')
+    console.log(error)
   } else {
-    const data = response.body
-    console.log(`${data.daily.summary} It is currently ${data.currently.temperature} degrees out. There is a ${data.currently.precipProbability*100}% chance of rain.`)
-  }
-})
+    console.log(geocode.location)
 
-const geoCodeToken = 'pk.eyJ1IjoidHJvbmdoaWV1IiwiYSI6ImNqd3hicWk5djBpNmYzeWp6ZXp1c3N3NHcifQ.kr1qW_AQ3AwdgPPFHqjI5Q'
-const address = 'Los%20Angeles'
-
-const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${geoCodeToken}&limit=1`
-
-request({ url: geoCodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to mapbox services!')
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location!')
-  } else {
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
-    console.log(`${latitude} : ${longitude}`)
+    forecast(geocode.latitude, geocode.longitude, (error, data) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(`${data.summary} It's ${data.temperature} degrees out. It have ${data.rainPercent}% chance of rain.`)
+      }
+    })
   }
 })
